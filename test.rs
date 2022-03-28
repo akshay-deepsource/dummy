ab33897aa8af5eb0cc7503372db0402278b0746d
Akshay <akshay@deepsource.io>
2022-03-28 15:52:13 +0530

    .

--- test.rs
+++ test.rs
@@ -1,37 +1,19 @@
-impl Error for MyType {
-    fn type_id(&self) -> TypeId { TypeId::of::<String>() }
-}
-
 fn main() {
-    let x = 2;
-    let x = 2;
-    let _ = Arc::get_mut(&mut abc);
-    let _ = std::fs::remove_dir_all("/some/path");
-    match p {
-        true => b(),
-        false => a(),
-    }
-
-    fn lorem(
-        ipsum: Ipsum,
-        dolor: Dolor,
-        sit: Sit,
-        amet: Amet,
-        consectetur: Consectetur,
-        adipiscing: Adipiscing,
-        elit: Elit,
-    ) {
-    }
-
-    let x: u32 = 0;
-    dbg!(x);
-
-    x.skip(2).next();
-    "abc".bytes().count();
-    iter.inspect(|x| { println!("{}", x); }).for_each(|f| {
-        f.x();
-    });
-    iter.step_by(0);
-    a.bytes().nth(2);
+    let x @ _ = 2;
 }
 
+trait X {
+    #[inline]
+    fn f() {};
+}
+
+#[must_use]
+fn f() -> () {
+    ()
+}
+
+type T = ();
+#[must_use]
+fn f() -> T {
+    ()
+}
